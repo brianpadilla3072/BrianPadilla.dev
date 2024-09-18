11e9d4b09e015eac8d33ecbcc2e88945c449fc70
brianpadilla3072 <padillabrian830@gmail.com>
2024-09-17 22:53:38 -0300

    config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
-import vercelServerless from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://porfolio.dev/',
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: vercel(),
 });
