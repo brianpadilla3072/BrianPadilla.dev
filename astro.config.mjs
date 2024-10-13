f96b71c144d3fa766a1ce89213b7ea212268de31
brianpadilla3072 <padillabrian830@gmail.com>
2024-10-13 09:09:06 -0300

    obtener datos proyects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import robotsTxt from 'astro-robots-txt';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(),react()],
+  integrations: [tailwind(), robotsTxt(),
+     react({
+    include: ['**/react/*'],
+    experimentalReactChildren: true,
+  })],
   site: 'https://porfolio.dev/',
   output: 'server',
   adapter: vercel(),
