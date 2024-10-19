41d060d04f45974a1c57c25230ac01ca312f90d9
brianpadilla3072 <padillabrian830@gmail.com>
2024-10-18 22:55:06 -0300

    footer

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -6,6 +6,8 @@ const MobileMenu = ({ navItems, id = "" }) => {
   const [isOpen, setIsOpen] = useState(false);
   let touchStartX = 0;
   let touchEndX = 0;
+  let touchStartY = 0; // Añadido para verificar el movimiento vertical
+  let touchEndY = 0;
 
   // Maneja la apertura del menú
   const openMenu = () => {
@@ -20,19 +22,33 @@ const MobileMenu = ({ navItems, id = "" }) => {
   // Detecta el inicio del gesto de deslizamiento
   const handleTouchStart = (e) => {
     touchStartX = e.changedTouches[0].screenX;
+    touchStartY = e.changedTouches[0].screenY; // Captura la posición vertical inicial
   };
 
   // Detecta el final del gesto de deslizamiento
   const handleTouchEnd = () => {
-    if (touchEndX > touchStartX + 50) {
-      openMenu(); // Abre el menú si se desliza hacia la derecha
-    } else if (touchEndX < touchStartX - 50) {
-      closeMenu(); // Cierra el menú si se desliza hacia la izquierda
+    const windowWidth = window.innerWidth;
+    const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY; // Diferencia vertical
+
+    // Verificar si el toque comenzó en la mitad izquierda (primer 50%) para abrir
+    if (touchStartX < windowWidth / 2 && Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX > 50) {
+        openMenu(); // Abre el menú si se desliza hacia la derecha desde la mitad izquierda
+      }
+    }
+
+    // Verificar si el toque comenzó en la mitad derecha (último 50%) para cerrar
+    if (touchStartX > windowWidth / 2 && Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX < -50) {
+        closeMenu(); // Cierra el menú si se desliza hacia la izquierda desde la mitad derecha
+      }
     }
   };
 
   const handleTouchMove = (e) => {
     touchEndX = e.changedTouches[0].screenX;
+    touchEndY = e.changedTouches[0].screenY; // Captura la posición vertical en movimiento
   };
 
   // Agrega los listeners para los eventos táctiles
@@ -52,8 +68,8 @@ const MobileMenu = ({ navItems, id = "" }) => {
     <>
       {/* Botón para abrir el menú */}
       <button
-        onClick={openMenu} // Cambiado a openMenu
-        className={`fixed top-4 right-4 z-20 inline-flex bg-gray-100 text-gray-800 border-gray-300 items-center justify-center gap-2 px-3 py-2 space-x-2 text-base transition dark:text-white dark:bg-gray-800 border dark:border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black text-white p-2 rounded-full shadow-lg focus:outline-none focus:ring-2 ring-blue-300 md:hidden 
+        onClick={openMenu}
+        className={`fixed top-4 right-4 z-20 inline-flex bg-gray-100 text-sm text-gray-950 border-gray-300 items-center justify-center gap-2 px-3 py-2 space-x-2 text-base transition dark:text-white dark:bg-gray-800 border dark:border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black text-white p-3 rounded-full shadow-lg focus:outline-none focus:ring-2 ring-blue-300 md:hidden 
         ${isOpen ? 'hidden' : 'block'}`}
         aria-label="Abrir menú"
       >
@@ -68,8 +84,6 @@ const MobileMenu = ({ navItems, id = "" }) => {
         }`}
         style={{ animation: 'nav-shadown 1s linear both' }}
       >
-       
-
         {navItems.map((link, index) => (
           <a
             key={index}
@@ -83,7 +97,6 @@ const MobileMenu = ({ navItems, id = "" }) => {
 
         <div className="flex justify-center items-center mt-4">
           <ButtonCv />
-          {/* <ThemeToggle /> */}
         </div>
       </nav>
     </>
